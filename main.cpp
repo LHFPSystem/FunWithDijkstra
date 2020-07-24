99e86325249a3b6d28bc5a05cbe7cfa42943dce9
LHFPSystem <41213056+LHFPSystem@users.noreply.github.com>
2020-07-23 23:58:21 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
 #include "Graph.h"
-#include "Lista.h"
+#include <iostream>
+using namespace std;
 
 int main(){
 
-    Graph grafo;
-    grafo.agregarVertice("EZE");
-    grafo.agregarVertice("BRC");
-    grafo.agregarVertice("LAX");
-    grafo.agregarVertice("JFK");
-    grafo.mostrarVertices();
-    bool resultado = grafo.agregarBorde("EZE","BRC",1,1);
-    cout << resultado << endl;
-    bool resultado2 = grafo.agregarBorde("EZE","SAS",1,1);
-    cout << resultado2 << endl;
+    Graph g(9);
+    g.agregarArista(0, 1, 4);
+    g.agregarArista(0, 7, 8);
+    g.agregarArista(1, 2, 8);
+    g.agregarArista(1, 7, 11);
+    g.agregarArista(2, 3, 7);
+    g.agregarArista(2, 8, 2);
+    g.agregarArista(2, 5, 4);
+    g.agregarArista(3, 4, 9);
+    g.agregarArista(3, 5, 14);
+    g.agregarArista(4, 5, 10);
+    g.agregarArista(5, 6, 2);
+    g.agregarArista(6, 7, 1);
+    g.agregarArista(6, 8, 6);
+    g.agregarArista(7, 8, 7);
+
+    g.dijkstra(1,4);
+
 
     return 0;
 }
